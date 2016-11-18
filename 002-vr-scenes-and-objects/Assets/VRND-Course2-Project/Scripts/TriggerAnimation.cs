a161e50605c2ea3b6d6bf23f0855e55d4bfc630a
Pablo Perotti <pablo@blackpixel.com>
2016-11-18 16:53:16 -0300

    chore: Ensure only GoogleVR & Plugins directories are only ignored inside AAssets folder

---
+++ 002-vr-scenes-and-objects/Assets/VRND-Course2-Project/Scripts/TriggerAnimation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerAnimation : MonoBehaviour {
+	[Tooltip("The name of the trigger parameter")]
+	public string AnimationName; //name of the trigger parameter
+	[Tooltip("The Animator Component we created")]
+	public Animator stateMachine; //animator state machine
+
+	void Update() {
+		if (GvrViewer.Instance.Triggered)
+			stateMachine.SetTrigger(AnimationName); //sets trigger
+	}
+
+}
