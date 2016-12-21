337932aca0e227203bb942b6287d8e0bdfcd42cc
Pablo Perotti <pablo@blackpixel.com>
2016-12-21 08:05:52 -0300

    feat: Add 1 coin (partially).

--- 003-vr-software-development/Assets/Udacity/Scripts/Coin.cs
+++ 003-vr-software-development/Assets/Udacity/Scripts/Coin.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Coin : MonoBehaviour 
 {
     //Create a reference to the CoinPoofPrefab
+	public GameObject coinPoofPrefab;
 
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
+
+
         // Make sure the poof animates vertically
+
         // Destroy this coin. Check the Unity documentation on how to use Destroy
+
     }
 
 }
