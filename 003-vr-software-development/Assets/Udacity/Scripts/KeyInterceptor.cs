f31bde23f19309c4a4d7992c9a9b616e773811a4
Pablo Perotti <pablo@blackpixel.com>
2016-12-22 09:53:11 -0300

    chore: Second final version.

---
+++ 003-vr-software-development/Assets/Udacity/Scripts/KeyInterceptor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInterceptor : MonoBehaviour 
+{
+	public GameObject key;
+
+	public void FixedUpdate()
+	{
+		//Vector3 fwd = transform.TransformDirection (Vector3.forward);
+		Vector3 fwd = key.transform.forward;
+
+		if(Physics.Raycast(transform.position, fwd, 10)) 
+		{
+			Debug.Log("Key Detected!");
+		}
+	}
+}
