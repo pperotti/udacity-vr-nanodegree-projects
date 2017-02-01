3be0707ccffe1b1c651e7fed56e416c43fe8d548
Pablo Perotti <pablo@blackpixel.com>
2017-02-01 15:35:01 -0300

    feat: Update station 1 details.

---
+++ 005-vr-applications/Assets/Station1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Station1 : MonoBehaviour {
+
+	public AudioSource audioSource;
+
+	public void roar(){
+		print ("play");
+		audioSource.Play();
+	}
+
+	public void next() {
+		print ("next");
+	}
+
+	public void prev() {
+		print ("prev");
+	}
+
+	public void quit() {
+		print ("quit");
+	}
+}
